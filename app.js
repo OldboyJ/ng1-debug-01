4cdb50b69ab5105c5f415da7dfcaf6fd3d2bc535
J Sandate <mrharisu@gmail.com>
2017-05-09 09:39:49 -0700

    debug warmup, make page work.

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-var app = angular.model('ourApp', {});
+var app = angular.module('ourApp', []);
 
 app.component('navBar', {
   template: `
     <nav class="navbar navbar-default">
       <div class="container-fluid">
         <div class="navbar-header">
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+          <button ng-click="$Ctrl.submitButton()" type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
             <span class="sr-only">Toggle navigation</span>
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
@@ -16,7 +16,7 @@ app.component('navBar', {
 
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
           <ul class="nav navbar-nav">
-            <li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
+            <li class="active"><a href="#">Link <span class="sr-only">current</span></a></li>
             <li><a href="#">Link</a></li>
           </ul>
           <ul class="nav navbar-nav navbar-right">
@@ -25,14 +25,19 @@ app.component('navBar', {
         </div>
       </div>
     </nav>
-  `
+  `,
+  controller: function() {
+    var vm = this;
+
+
+  }
 })
 
-app.comonent('theForm', {
+app.component('theForm', {
   template: `
     <p>Enter your name: <input type="text" ng-model="name"></p>
     <p>Hello: {{name}}</p>
-  `
+  `,
   controller: function() {
     var vm = this;
   }
